f60d09259b61c53f8960b2c9870af1194d961f1d
Something-Debug <54156973+Something-Debug@users.noreply.github.com>
2020-08-02 21:10:58 -0500

    Added discord logging & moved checks to the server

--- sv_blacklist.lua
+++ sv_blacklist.lua
@@ -1,44 +1,77 @@
+local webhook = ''
+function log(message)
+    local serverName = GetConvar('sv_hostname', '')
+    local embed = {
+      {
+        ['title'] = 'Blacklist on '..serverName,
+        ['type'] = 'rich',
+        ['description'] = message,
+        ['color'] = 732633,
+        ['footer'] = {['text'] = os.date("%x (%X %p)")}
+      }
+    }
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
+
 local _blacklistedmodels = {
-    [`cargoplane`] = 'cargoplane'
-}
-local _blacklistedNaturalSpawn = {
-    [`police3`] = 'police3'
+    [`adder`] = true,
+    [`cargoplane`] = true
 }
 AddEventHandler('entityCreating', function(entity)
     local entityOwner = NetworkGetEntityOwner(entity)
-    if not IsPlayerAceAllowed(entityOwner, 'command') then --Check permissions
-        if _blacklistedmodels[GetEntityModel(entity)] then --Is the vehicle being spawned blacklisted
-            TriggerClientEvent('notify', entityOwner, 'Blacklisted Vehicle Model') --Let the client know the vehicle is blacklisted
-            CancelEvent() --Cancel the creating of the vehicle
-        end
-        if _blacklistedNaturalSpawn[GetEntityModel(entity)] then --Is the vehicle being spawned blacklisted
-            CancelEvent() --Cancel the creating of the vehicle
+    if not IsPlayerAceAllowed(entityOwner, 'Blacklist') then
+        if _blacklistedmodels[GetEntityModel(entity)] then
+            CancelEvent()
+            TriggerClientEvent('notify', entityOwner, 'Blacklisted Vehicle') 
         end
     end
 end)
 
---All code below was taken from https://github.com/Zeemahh/no-god-mode just changed the event name. Thanks Zeemahh
-function ProcessAces()
-    if GetNumPlayerIndices() > 0 then -- don't do it when there aren't any players
-        for i=0, GetNumPlayerIndices()-1 do -- loop through all players
-            player = tonumber(GetPlayerFromIndex(i))
-            Citizen.Wait(0)
-            if IsPlayerAceAllowed(player, 'Blacklist') then
-                TriggerClientEvent("checkAce", player, true)
-            end
-        end
-    end
-end
+local _blacklistedPeds = {
+    'a_m_m_acult_01',
+    'a_m_y_acult_01'
+}
 
+local _blacklistedWeapons = {
+    'WEAPON_RPG',
+    'WEAPON_SMG'
+}
+
+local _blacklistedVehicles = {
+    'adder',
+    'cargoplane'
+}
 CreateThread(function()
     while true do
-        ProcessAces()
-        Wait(60000) --Check every minute
-    end
-end)
-
-AddEventHandler("onResourceStart", function(name)
-    if name == GetCurrentResourceName() then
-        ProcessAces()
+        Wait(1000)
+        if GetNumPlayerIndices() > 0 then
+            local player = GetPlayerFromIndex(0)
+            local playerPed = GetPlayerPed(player)
+            local coords = GetEntityCoords(playerPed)
+            local name = GetPlayerName(player)
+            if not IsPlayerAceAllowed(player, 'Blacklist') then
+                for _, pedNames in pairs(_blacklistedPeds) do
+                    if GetEntityModel(playerPed) == GetHashKey(pedNames) then
+                        TriggerClientEvent('setPed', player)
+                        log('**Player:** `' ..name..'`\n**Information:** Tried to set themselves as a blacklisted ped `' ..pedNames..'`\n**Coords:** `' ..coords..'`')
+                    end
+                end
+                for _, wepNames in pairs(_blacklistedWeapons) do
+                    if GetSelectedPedWeapon(playerPed) == GetHashKey(wepNames) then
+                        RemoveWeaponFromPed(playerPed, wepNames)
+                        log('**Player:** `' ..name..'`\n **Information:** Tried to use a blacklisted weapon `' ..wepNames..'`\n**Coords:** `' ..coords..'`')
+                        TriggerClientEvent('notify', player, 'Blacklisted Weapon')
+                    end
+                end
+                for _, vehNames in pairs(_blacklistedVehicles) do
+                    local vehicle = GetVehiclePedIsIn(playerPed, false)
+                    if GetEntityModel(vehicle) == GetHashKey(vehNames) then
+                        DeleteEntity(vehicle)
+                        log('**Player:** `' ..name..'`\n **Information:** Tried to spawn a blacklisted vehicle `' ..vehNames.. '`\n**Coords:** `' ..coords..'`')
+                        TriggerClientEvent('notify', player, 'Blacklisted Vehicle')    
+                    end
+                end
+            end
+        end
     end
 end)
